5858e3ae47d7d7ceb3a90d97514444eeed82c15a
Greg Gibeling <gdgib@outlook.com>
2018-10-30 21:38:34 -0700

    G2-287 gb-functional SSHRunner

--- gb-functional/src/test/java/com/g2forge/gearbox/functional/TestProxifier.java
+++ gb-functional/src/test/java/com/g2forge/gearbox/functional/TestProxifier.java
@@ -11,46 +11,38 @@ import org.junit.Test;
 import com.g2forge.alexandria.java.core.helpers.HArray;
 import com.g2forge.alexandria.java.core.helpers.HString;
 import com.g2forge.gearbox.functional.control.Command;
-import com.g2forge.gearbox.functional.proxy.Proxifier;
+import com.g2forge.gearbox.functional.runner.ATestRunner;
 import com.g2forge.gearbox.functional.runner.IProcess;
 import com.g2forge.gearbox.functional.runner.IRunner;
 import com.g2forge.gearbox.functional.runner.ProcessBuilderRunner;
 
-public class TestProxifier {
+public class TestProxifier extends ATestRunner {
 	public interface ForTesting {
 		@Command("false")
 		public IProcess false_();
 	}
 
-	protected final Proxifier proxifier = new Proxifier();
-
-	protected final IRunner runner = new ProcessBuilderRunner(null);
-
-	protected final IUtils utils = proxifier.generate(runner, IUtils.class);
-
 	@Test(expected = RuntimeException.class)
 	public void assertSuccess() {
-		proxifier.generate(runner, ForTesting.class).false_().assertSuccess();
+		proxifier.generate(getRunner(), ForTesting.class).false_().assertSuccess();
+	}
+
+	protected IRunner createRunner() {
+		return new ProcessBuilderRunner(null);
 	}
 
 	@Test
 	public void echo() {
 		final String[] args = HArray.create("foo\\n!", "bar");
 		final String expected = HString.unescape(Stream.of(args).collect(Collectors.joining(" "))) + "\n";
-		final String actual = utils.echo(true, args);
+		final String actual = getUtils().echo(true, args);
 		Assert.assertEquals(expected, actual);
 	};
 
 	@Test
-	public void exitcode() {
-		Assert.assertFalse(utils.false_());
-		Assert.assertTrue(utils.true_());
-	}
-
-	@Test
 	public void pwd() {
 		final Path path = Paths.get("").toAbsolutePath();
-		final String result = utils.pwd(path, true).trim();
+		final String result = getUtils().pwd(path, true).trim();
 		final int slash = result.lastIndexOf('/');
 		final String actual = (slash >= 0) ? result.substring(slash + 1) : result;
 		Assert.assertEquals(path.getFileName().toString(), actual);
---
+++ gb-functional/src/test/java/com/g2forge/gearbox/functional/runner/TestSSHRunner.java
@@ -0,0 +1,43 @@
+package com.g2forge.gearbox.functional.runner;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.g2forge.alexandria.java.close.ICloseable;
+import com.g2forge.alexandria.wizard.PropertyStringInput;
+import com.g2forge.alexandria.wizard.UserPasswordInput;
+import com.g2forge.alexandria.wizard.UserStringInput;
+import com.g2forge.gearbox.functional.runner.SSHServer.SSHServerBuilder;
+
+import lombok.Getter;
+
+public class TestSSHRunner extends ATestRunner {
+	@Getter(lazy = true)
+	private static final SSHServer server = createServer();
+
+	protected static SSHServer createServer() {
+		final SSHServerBuilder builder = SSHServer.builder();
+		builder.username(new PropertyStringInput("ssh.username").fallback(new UserStringInput("SSH Username", false)).get());
+		builder.password(new PropertyStringInput("ssh.password").fallback(new UserPasswordInput("SSH Password")).get());
+		builder.host(new PropertyStringInput("ssh.host").fallback(new UserStringInput("SSH Host", false)).get());
+		builder.port(Integer.valueOf(new PropertyStringInput("ssh.port").fallback(new UserStringInput("SSH Port", false)).get()));
+		final SSHServer server = builder.build();
+		return server;
+	}
+
+	@After
+	public void close() {
+		((ICloseable) getRunner()).close();
+	}
+
+	@Override
+	protected IRunner createRunner() {
+		return new SSHRunner(getServer());
+	}
+
+	@Test
+	public void test() {
+		Assert.assertEquals(new PropertyStringInput("ssh.hostname").fallback(new UserStringInput("SSH Hostname", false)).get(), getUtils().echo(false, "${HOSTNAME}").trim());
+	}
+}
