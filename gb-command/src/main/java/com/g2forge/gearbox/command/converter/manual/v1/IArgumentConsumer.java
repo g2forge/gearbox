6284d94f31108f8a9c2677b08baa00bc22ed002d
Greg Gibeling <gdgib@outlook.com>
2019-06-12 23:10:58 -0700

    G2-312 Fix typing for older versions of Javac

--- gb-command/src/main/java/com/g2forge/gearbox/command/converter/manual/v1/IArgumentConsumer.java
+++ gb-command/src/main/java/com/g2forge/gearbox/command/converter/manual/v1/IArgumentConsumer.java
@@ -8,6 +8,7 @@ import com.g2forge.alexandria.metadata.MetadataLoader;
 import com.g2forge.gearbox.command.converter.IMethodArgument;
 import com.g2forge.gearbox.command.proxy.method.MethodInvocation;
 import com.g2forge.gearbox.command.proxy.process.ProcessInvocation;
+import com.g2forge.gearbox.command.proxy.process.ProcessInvocation.ProcessInvocationBuilder;
 
 @MetadataLoader(IArgumentConsumer.ArgumentConsumerMetadataLoader.class)
 @FunctionalInterface
@@ -18,15 +19,18 @@ public interface IArgumentConsumer extends IConsumer3<ProcessInvocation.ProcessI
 			return IMetadataLoader.load(type, metadata, IArgumentConsumer.class, m -> {
 				final ArgumentConsumers argumentConsumers = m.getMetadata(ArgumentConsumers.class);
 				if (argumentConsumers == null) return null;
-				return (processInvocationBuilder, methodInvocation, argument) -> {
-					for (ArgumentConsumer argumentConsumerMetadata : argumentConsumers.value()) {
-						final IConsumer3<ProcessInvocation.ProcessInvocationBuilder<Object>, MethodInvocation, IMethodArgument<?>> argumentConsumer;
-						try {
-							argumentConsumer = argumentConsumerMetadata.value().newInstance();
-						} catch (InstantiationException | IllegalAccessException e) {
-							throw new RuntimeReflectionException(e);
+				return new IArgumentConsumer() {
+					@Override
+					public void accept(ProcessInvocationBuilder<Object> processInvocationBuilder, MethodInvocation methodInvocation, IMethodArgument<?> argument) {
+						for (ArgumentConsumer argumentConsumerMetadata : argumentConsumers.value()) {
+							final IConsumer3<ProcessInvocation.ProcessInvocationBuilder<Object>, MethodInvocation, IMethodArgument<?>> argumentConsumer;
+							try {
+								argumentConsumer = argumentConsumerMetadata.value().newInstance();
+							} catch (InstantiationException | IllegalAccessException e) {
+								throw new RuntimeReflectionException(e);
+							}
+							argumentConsumer.accept(processInvocationBuilder, methodInvocation, argument);
 						}
-						argumentConsumer.accept(processInvocationBuilder, methodInvocation, argument);
 					}
 				};
 			});
