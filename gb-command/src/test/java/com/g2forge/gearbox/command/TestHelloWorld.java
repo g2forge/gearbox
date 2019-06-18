9e5c1698d47aae0135cbd3d7d1a7c3683e9ad889
Greg Gibeling <gdgib@outlook.com>
2019-06-17 21:29:43 -0700

    G2-491 Comments

--- gb-command/src/test/java/com/g2forge/gearbox/command/TestHelloWorld.java
+++ gb-command/src/test/java/com/g2forge/gearbox/command/TestHelloWorld.java
@@ -10,15 +10,32 @@ import com.g2forge.gearbox.command.proxy.ICommandProxyFactory;
 import com.g2forge.gearbox.command.proxy.method.ICommandInterface;
 
 public class TestHelloWorld {
+	/**
+	 * A command interface for "echo". Note that the {@link ICommandInterface} parent is empty, and totally optional.
+	 */
 	public interface IEcho extends ICommandInterface {
+		/**
+		 * Run the "echo" command over the specified string arguments. Note that no annotations are necessary to control command line generation, because "echo"
+		 * is such a simple command. For more complex commands you may need the annotations that go with {@link DumbCommandConverter}.
+		 * 
+		 * @param args The arguments to echo
+		 * @return The output of echo from standard out.
+		 */
 		public String echo(String... args);
 	}
 
 	@Test
 	public void helloWorld() {
+		// Create a new command proxy factory
+		// Use the "dumb" converter to render command line arguments from method arguments
+		// Use process builder to run the resulting commands
 		final ICommandProxyFactory factory = new CommandProxyFactory(DumbCommandConverter.create(), new ProcessBuilderRunner(null));
+
+		// Get a magical implementation of the interface
 		final IEcho echo = factory.apply(IEcho.class);
+
+		// Make sure it works correctly
 		final String string = "Hello, World!";
-		Assert.assertEquals(string, echo.echo(string).trim());
+		Assert.assertEquals(string, echo.echo(string).trim(/* We want to trim the new line that echo adds */));
 	}
 }
