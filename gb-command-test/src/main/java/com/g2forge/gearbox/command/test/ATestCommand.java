940f430548c5a4fd62761d7ad0ed9c15dd93cdb7
Greg Gibeling <gdgib@outlook.com>
2021-02-27 14:15:19 -0800

    G2-1148 Fix javac weirdness

--- gb-command-test/src/main/java/com/g2forge/gearbox/command/test/ATestCommand.java
+++ gb-command-test/src/main/java/com/g2forge/gearbox/command/test/ATestCommand.java
@@ -19,13 +19,17 @@ import lombok.Getter;
 
 public abstract class ATestCommand {
 	@Getter(lazy = true)
-	private final ICommandProxyFactory factory = new CommandProxyFactory(createRenderer(), getRunner());
+	private final ICommandProxyFactory factory = computeFactory();
 
 	@Getter(lazy = true)
 	private final IFunction1<CommandInvocation<IRedirect, IRedirect>, IProcess> runner = createRunner();
 
 	@Getter(lazy = true)
 	private final IUtils utils = getFactory().apply(IUtils.class);
+
+	protected ICommandProxyFactory computeFactory() {
+		return new CommandProxyFactory(createRenderer(), getRunner());
+	}
 
 	protected ICommandConverterR_ createRenderer() {
 		return null;
