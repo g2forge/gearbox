f64efe4b9110eae8e4a2e7202a9f34296d353c85
Greg Gibeling <gdgib@outlook.com>
2021-12-24 12:25:41 -0800

    G2-1275 Improve failed process logging

--- gb-command/src/main/java/com/g2forge/gearbox/command/process/IProcess.java
+++ gb-command/src/main/java/com/g2forge/gearbox/command/process/IProcess.java
@@ -2,20 +2,34 @@ package com.g2forge.gearbox.command.process;
 
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.g2forge.alexandria.command.stdio.IStandardIO;
 import com.g2forge.alexandria.java.close.ICloseable;
+import com.g2forge.alexandria.java.core.error.HError;
+import com.g2forge.alexandria.java.function.IConsumer1;
 import com.g2forge.alexandria.java.io.HTextIO;
 
 public interface IProcess extends ICloseable, IStandardIO<OutputStream, InputStream> {
 	public default void assertSuccess() {
 		if (!isSuccess()) {
 			final StringBuilder builder = new StringBuilder();
+			final List<Throwable> throwables = new ArrayList<>();
+			final IConsumer1<? super String> consumer = line -> builder.append('\t').append(line).append('\n');
 			builder.append("Standard output:\n");
-			HTextIO.readAll(getStandardOutput()).forEach(line -> builder.append('\t').append(line).append('\n'));
+			try {
+				HTextIO.readAll(getStandardOutput(), consumer);
+			} catch (Throwable throwable) {
+				throwables.add(throwable);
+			}
 			builder.append("Standard error:\n");
-			HTextIO.readAll(getStandardError()).forEach(line -> builder.append('\t').append(line).append('\n'));
-			throw new RuntimeException(builder.toString());
+			try {
+				HTextIO.readAll(getStandardError(), consumer);
+			} catch (Throwable throwable) {
+				throwables.add(throwable);
+			}
+			throw HError.withSuppressed(new RuntimeException(builder.toString()), throwables);
 		}
 	}
 
