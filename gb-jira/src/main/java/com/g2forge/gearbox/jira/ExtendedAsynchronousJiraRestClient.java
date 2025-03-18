257cc206dd867c50df87538fa7312a369f84601d
Greg Gibeling <gdgib@outlook.com>
2025-03-18 16:28:21 -0700

    G2-1740 Add support for querying Jira users by key

--- gb-jira/src/main/java/com/g2forge/gearbox/jira/ExtendedAsynchronousJiraRestClient.java
+++ gb-jira/src/main/java/com/g2forge/gearbox/jira/ExtendedAsynchronousJiraRestClient.java
@@ -8,6 +8,8 @@ import com.atlassian.jira.rest.client.internal.async.AsynchronousJiraRestClient;
 import com.atlassian.jira.rest.client.internal.async.DisposableHttpClient;
 import com.g2forge.gearbox.jira.components.AsynchronousProjectComponentClient;
 import com.g2forge.gearbox.jira.components.ProjectComponentsRestClient;
+import com.g2forge.gearbox.jira.user.AsynchronousExtendedUserRestClient;
+import com.g2forge.gearbox.jira.user.ExtendedUserRestClient;
 
 import lombok.Getter;
 
@@ -15,9 +17,13 @@ public class ExtendedAsynchronousJiraRestClient extends AsynchronousJiraRestClie
 	@Getter
 	protected final ProjectComponentsRestClient projectComponentsClient;
 
+	@Getter
+	protected final ExtendedUserRestClient userClient;
+
 	public ExtendedAsynchronousJiraRestClient(URI serverUri, DisposableHttpClient httpClient) {
 		super(serverUri, httpClient);
 		final URI baseUri = UriBuilder.fromUri(serverUri).path("/rest/api/latest").build();
 		projectComponentsClient = new AsynchronousProjectComponentClient(baseUri, httpClient);
+		userClient = new AsynchronousExtendedUserRestClient(baseUri, httpClient);
 	}
 }
