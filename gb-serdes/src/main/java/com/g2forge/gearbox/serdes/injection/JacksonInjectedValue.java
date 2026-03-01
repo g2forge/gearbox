fca43bd203be8e473ba76d3a46cbc8f62ff7a762
Greg Gibeling <gdgib@outlook.com>
2026-03-01 12:15:45 -0800

    G2-1881 Support databind context & mapper

--- gb-serdes/src/main/java/com/g2forge/gearbox/serdes/injection/JacksonInjectedValue.java
+++ gb-serdes/src/main/java/com/g2forge/gearbox/serdes/injection/JacksonInjectedValue.java
@@ -1,12 +1,9 @@
 package com.g2forge.gearbox.serdes.injection;
 
-import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.DatabindContext;
 import com.fasterxml.jackson.databind.InjectableValues;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationConfig;
-import com.fasterxml.jackson.databind.SerializerProvider;
-import com.fasterxml.jackson.databind.cfg.ContextAttributes;
 import com.fasterxml.jackson.databind.exc.MissingInjectableValueExcepion;
 import com.g2forge.alexandria.java.type.ref.ITypeRef;
 
@@ -32,30 +29,16 @@ public class JacksonInjectedValue<T> {
 		this(type.getName(), ITypeRef.of(type), fallback);
 	}
 
-	public ObjectMapper inject(ObjectMapper mapper, T value) {
-		final SerializationConfig initialSerializationConfig = mapper.getSerializationConfig();
-		final ContextAttributes attributes = initialSerializationConfig.getAttributes();
-		final ContextAttributes attributesWithInjectedValue = attributes.withSharedAttribute(getId(), value);
-		final SerializationConfig serializationConfigWithInjectedValue = initialSerializationConfig.with(attributesWithInjectedValue);
-
-		final InjectableValues existingInjectableValues = mapper.getInjectableValues();
-		if (existingInjectableValues == null) mapper.setInjectableValues(new InjectableValues.Std().addValue(getId(), value));
-		else if (!(existingInjectableValues instanceof InjectableValues.Std)) throw new IllegalArgumentException();
-		else((InjectableValues.Std) existingInjectableValues).addValue(getId(), value);
-
-		return mapper.setConfig(serializationConfigWithInjectedValue);
-	}
-
-	public T get(SerializerProvider provider) {
-		final Object value = provider.getAttribute(getId());
+	public T get(DatabindContext context) {
+		final Object value = context.getAttribute(getId());
 		if (value == null) return getFallback();
 		return getType().cast(value);
 	}
 
-	public T get(DeserializationContext context) {
+	public T get(ObjectMapper mapper) {
 		final Object value;
 		try {
-			value = context.findInjectableValue(getId(), null, null, null, null);
+			value = mapper.getInjectableValues().findInjectableValue(mapper.getDeserializationContext(), getId(), null, null, null, null);
 		} catch (MissingInjectableValueExcepion e) {
 			return getFallback();
 		} catch (JsonMappingException e) {
@@ -63,5 +46,17 @@ public class JacksonInjectedValue<T> {
 		}
 		if (value == null) return getFallback();
 		return getType().cast(value);
+	}
+
+	public ObjectMapper inject(ObjectMapper mapper, T value) {
+		mapper.setConfig(mapper.getSerializationConfig().withAttribute(getId(), value));
+		mapper.setConfig(mapper.getDeserializationConfig().withAttribute(getId(), value));
+
+		final InjectableValues existingInjectableValues = mapper.getInjectableValues();
+		if (existingInjectableValues == null) mapper.setInjectableValues(new InjectableValues.Std().addValue(getId(), value));
+		else if (!(existingInjectableValues instanceof InjectableValues.Std)) throw new IllegalArgumentException();
+		else((InjectableValues.Std) existingInjectableValues).addValue(getId(), value);
+
+		return mapper;
 	}
 }
